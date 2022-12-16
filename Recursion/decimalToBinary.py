144605d72d9aaa91a61c3da69fdf73db708a1c60
Khan2406 <82305740+khan215701@users.noreply.github.com>
2022-12-16 11:31:28 +0530

    recursion - decimal to binary

---
+++ Recursion/decimalToBinary.py
@@ -0,0 +1,13 @@
+import sys
+sys.setrecursionlimit(100)
+
+
+def decimalToBinary(n):
+    assert int(n) == n, 'number must be integer.'
+    if n == 0:
+        return 0
+    return n % 2 + 10 * decimalToBinary(int(n/2))
+
+
+result = decimalToBinary(10)
+print(result)
